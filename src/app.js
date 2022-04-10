7aab317e58c2c7c27a7820975b2dc1a2b3bb8ad1
Shaid Islam <shaid85@gmail.com>
2022-04-10 12:22:41 +0600

    Restful api , text with Postman app

--- src/app.js
+++ src/app.js
@@ -5,12 +5,13 @@ require("./db/conn")
 const app = express()
 const port = process.env.PORT || 3000
 
+// Create Router
 const mensRouter = require("./routers/mendata")
 
 app.use(express.json())
 app.use(mensRouter)
 
-
+//  Connection 
 app.listen(port, () => {
     console.log(`Connection is live at port ${port}`);
 })
